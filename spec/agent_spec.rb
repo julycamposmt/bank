36a1fed290b1bded043d52f26b105a468d879b18
ingbus <julycamposmt@hotmail.com>
2020-09-14 22:36:20 +0200

    Subo la estructura del proyecto al repositorio de GitHub

--- spec/agent_spec.rb
+++ spec/agent_spec.rb
@@ -4,40 +4,40 @@ require './lib/banking'
 describe Banking::Agent do
 
 	before :each do
-		@bank       = Banking::Bank.new( "Test_Bank" )
-		@other_bank = Banking::Bank.new( "Other_Bank" )
-		@client1    = Banking::Client.new("Test_client1")
-		@client2    = Banking::Client.new("Test_client2")
-		@account1   = @bank.createAccount(@client1)
-		@account2   = @other_bank.createAccount(@client2)
-		@account3   = @bank.createAccount(@client2)
+		@bank          = Banking::Bank.new( "Test_Bank" )
+		@client_one    = Banking::Client.new("Test_Client_One")
+		@client_two    = Banking::Client.new("Test_Client_Two")
+		@other_bank    = Banking::Bank.new( "Other_Bank" )
+		@account_one   = @bank.createAccount(@client_one)
+		@account_two   = @bank.createAccount(@client_two)
+		@account_three = @other_bank.createAccount(@client_two)
 	end
 	describe 'Transfers' do 
 
 		context 'Transfers between two accounts, same banks' do
 			it 'Sucessfull transfer' do
-				@account1.addFounds 30000
-				Banking::Agent.new().transfer @account1, @account3, 30000
-				expect(@account1.balance).to be (30000 - 12000 )
-				expect(@account3.balance).to be 12000
+				@account_two.addFounds 30000
+				Banking::Agent.new().transfer @account_two, @account_one, 30000
+				expect(@account_two.balance).to be (30000 - 12000 )
+				expect(@account_one.balance).to be 12000
 			end
 			it 'Not enough funds' do
-				@account1.addFounds 30000
-				expect{Banking::Agent.new().transfer @account1, @account3, 100000}.to raise_error
+				@account_two.addFounds 30000
+				expect{Banking::Agent.new().transfer @account_two, @account_one, 100000}.to raise_error
 			end
 
 		end
 
 		context 'Transfers between two accounts, different banks' do
 			it 'Sucessfull transfer' do
-				@account1.addFounds 30000
-				Banking::Agent.new().transfer @account1, @account2, 12000
-				expect(@account1.balance).to be (30000 - ( 12000 + (12000/1000.to_f).ceil*5 ) )
-				expect(@account2.balance).to be 12000
+				@account_one.addFounds 30000
+				Banking::Agent.new().transfer @account_one, @account_three, 12000
+				expect(@account_one.balance).to be (30000 - ( 12000 + (12000/1000.to_f)*5 ) )
+				expect(@account_three.balance).to be 12000
 			end
 			it 'Not enough funds' do
-				@account1.addFounds 30000
-				expect{Banking::Agent.new().transfer @account1, @account2, 12000}.to raise_error
+				@account_one.addFounds 30000
+				expect{Banking::Agent.new().transfer @account_one, @account_three, 12000}.to raise_error
 			end
 
 		end
